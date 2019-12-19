019a54dc06c58582c9b8835f06bb7db43cbecb51
Diogo Zimmermann <diogo.zmm@gmail.com>
2019-12-19 10:32:37 -0300

    Code ready

---
+++ PiEmNDigitos/Program.cs
@@ -0,0 +1,33 @@
+﻿/* PROGRAMA: Número PI em N casas decimais
+ * Autor: Diogo Zimmermann
+ * Data: 19/12/2019
+ * 
+ * O programa mostra o número PI com um número 
+ * determinado pelo usuário de casas decimais.
+ * 
+ */
+
+using System;
+
+namespace PiEmNDigitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor (Entre 1 e 15)");
+            try
+            {
+                int value = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Round(Math.PI, value));
+            }
+
+            catch (Exception)
+            {
+                Console.WriteLine("Digite um valor entre 1 e 15!");
+            }
+          
+        }
+
+    }
+}
